74f6a62cb238f8b8de83eb4160c103b516735f9e
Serhii Korobchenko <jchild2008@gmail.com>
2022-09-29 13:26:20 +0300

    290922

---
+++ Lectures/Proxy.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+
+class Request(ABC):
+    @abstractmethod
+    def request(self) -> None:
+        pass
+
+
+class RealRequest(Request):
+    def request(self) -> None:
+        print("RealRequest: Handling request.")
+
+
+class Proxy(Request):
+    def __init__(self, real_request) -> None:
+        self._real_request = real_request
+
+    def request(self) -> None:
+        self._real_request.request()
+
+        
+        self.log_access()
+
+    def log_access(self) -> None:
+        print("Proxy: Logging the time of request.")
+
+
+def client_code(subject) -> None:
+    subject.request()
+
+
+if __name__ == "__main__":
+    proxy = Proxy(RealRequest())
+    client_code(proxy)
